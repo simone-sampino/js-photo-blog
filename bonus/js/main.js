62a991d8df148f95378846c49a1a2d6aac183ae8
simone-sampino <s.samp98@gmail.com>
2025-06-18 09:34:22 +0200

    Add initial HTML structure, CSS styles, and JavaScript for dynamic card generation

---
+++ bonus/js/main.js
@@ -0,0 +1,31 @@
+const rowEl = document.querySelector(".row");
+
+const endPointUrl = "https://lanciweb.github.io/demo/api/pictures/";
+
+fetch(endPointUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+      const card = data[i];
+      //   console.log(card);
+      const { title, date, url } = card;
+      console.log(title, date, url);
+
+      const cardMarkup = `
+        <div class="col-4 col-md-6 col-sm-12">
+            <div class="card">
+                <img class="pin" src="./assets/img/pin.svg" alt="">
+                <img src="${url}" alt="">
+                <div class="card-body">
+                    <div>${title}</div>
+                    <div>${date}</div>
+                </div>
+            </div>
+        </div>
+      `;
+
+      rowEl.insertAdjacentHTML("beforeend", cardMarkup);
+    }
+  });
