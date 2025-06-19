e6b205356eb6b980df135ea68d242e579cfeff16
simone-sampino <s.samp98@gmail.com>
2025-06-19 18:54:11 +0200

    Refactor modal implementation: move modal styles to CSS, update markup in JS, and enhance card display

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,14 +13,15 @@ fetch(endPointUrl)
       const { title, date, url } = card;
       console.log(title, date, url);
 
+      // add card markup in html from js
       const cardMarkup = `
         <div class="col col-sm-12 col-md-6 col-lg-4">
             <div class="card">
                 <img class="pin" src="./assets/img/pin.svg" alt="">
-                <img src="${url}" alt="">
+                <img class="card-img" src="${url}" alt="">
                 <div class="card-body">
-                    <div>${title}</div>
-                    <div>${date}</div>
+                  <div>${date}</div>
+                  <div class="title">${title}</div>
                 </div>
             </div>
         </div>
@@ -28,4 +29,25 @@ fetch(endPointUrl)
 
       rowEl.insertAdjacentHTML("beforeend", cardMarkup);
     }
+
+    // add modal markup in html from js
+    const modalMarkup = `
+      <div class="overlay d-none">
+          <div class="modal">
+              <img src="./assets/img/grogu.jpg" alt="">
+              <button><i class="fa-solid fa-xmark fa-3x"></i></button>
+          </div>
+      </div>
+      `;
+
+    rowEl.insertAdjacentHTML("afterend", modalMarkup);
+
+    const overlayEl = document.querySelector(".overlay");
+    const cardImgEl = document.querySelector(".card-img");
+    console.log(cardImgEl);
+
+    // add event listener to show overlay
+    cardImgEl.addEventListener("click", () => {
+      overlayEl.classList.remove("d-none");
+    });
   });
