eb1034206d5f34b0c9ef9da06fe45fdc0ea4d1f3
simone-sampino <s.samp98@gmail.com>
2025-06-19 20:35:03 +0200

    Fix card image alt attribute and improve modal markup structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ fetch(endPointUrl)
         <div class="col col-sm-12 col-md-6 col-lg-4">
             <div class="card">
                 <img class="pin" src="./assets/img/pin.svg" alt="">
-                <img class="card-img" src="${url}" alt="">
+                <img class="card-img" src="${url}" alt="${title}">
                 <div class="card-body">
                   <div>${date}</div>
                   <div class="title">${title}</div>
@@ -28,29 +28,38 @@ fetch(endPointUrl)
       `;
 
       rowEl.insertAdjacentHTML("beforeend", cardMarkup);
+
+      // add modal markup in html from js
+      const modalMarkup = `
+        <div class="overlay d-none">
+            <div class="modal">
+                <img class="modal-img" src="./assets/img/grogu.jpg" alt="grogu">
+                <button><i class="fa-solid fa-xmark fa-3x"></i></button>
+            </div>
+        </div>
+        `;
+
+      rowEl.insertAdjacentHTML("afterend", modalMarkup);
     }
-
-    // add modal markup in html from js
-    const modalMarkup = `
-      <div class="overlay d-none">
-          <div class="modal">
-              <img src="./assets/img/grogu.jpg" alt="">
-              <button><i class="fa-solid fa-xmark fa-3x"></i></button>
-          </div>
-      </div>
-      `;
-
-    rowEl.insertAdjacentHTML("afterend", modalMarkup);
 
     // DOM elements
     const overlayEl = document.querySelector(".overlay");
-    const cardImgEl = document.querySelector(".card-img");
+    const cardImgEl = document.querySelectorAll(".card-img");
     // console.log(cardImgEl);
     const btnEl = document.querySelector("button");
+    const modalImgEl = document.querySelector(".modal-img");
+    // console.log(modalImgEl);
 
     // add event listener to show overlay
-    cardImgEl.addEventListener("click", () => {
-      overlayEl.classList.remove("d-none");
+    cardImgEl.forEach((img) => {
+      img.addEventListener("click", () => {
+        overlayEl.classList.remove("d-none");
+
+        modalImgEl.src = img.src;
+        modalImgEl.alt = img.alt;
+      });
+      console.log(img.src);
+      console.log(img.alt);
     });
 
     // add event listener to hide overlay
